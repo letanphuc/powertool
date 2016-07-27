56d9a9ea07e13603732fbef3c78588743c544cda
duydn1 <duy.dn89@gmail.com>
2016-07-27 23:24:34 +0700

    add change language

---
+++ WebSensor/languages/lang.en.php
@@ -0,0 +1,66 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+$lang['PAGE_TITLE'] = 'ViLabs';
+
+//General:
+$lang['PROPERTIES_NAME'] = 'Name:';
+$lang['PROPERTIES_DESCRIPTION'] = 'Description:';
+$lang['PROPERTIES_NOTE'] = 'Note:';
+
+//About
+$lang['INFORMATION_ABOUT'] = 'About';
+$lang['INFORMATION_UPDATE'] = 'Update';
+$lang['INFORMATION_CHOOSE_FILE'] = 'Choose xxxxfile';
+
+//System information
+$lang['SYSTEM_INFO'] = 'System Infos';
+$lang['SYSTEM_INFO_TYPE'] = 'Type:';
+
+//Live View Properties
+$lang['LIVE_VIEW_PROPERTIES'] = 'Live view properties';
+
+//Column Properties
+$lang['COLUMN_PROPERTIES'] = 'Column Properties';
+$lang['COLUMN_PROPERTIES_TYPE'] = 'Type:';
+$lang['COLUMN_PROPERTIES_AVG'] = 'Avg:';
+$lang['COLUMN_PROPERTIES_MAX'] = 'Max:';
+$lang['COLUMN_PROPERTIES_MIN'] = 'Min:';
+$lang['COLUMN_PROPERTIES_AB_ER'] = 'Ab Er:';
+$lang['COLUMN_PROPERTIES_RE_ER'] = 'Re Er:';
+$lang['COLUMN_PROPERTIES_EQUATION'] = 'Equation:';
+
+//Tabs
+$lang['TAB_LIVEVIEW'] = 'Live View';
+$lang['TAB_DATA_TABLE'] = 'Data Table';
+
+//Button
+$lang['BUTTON_ADD_GRAPH'] = 'Add Graph';
+$lang['BUTTON_START_COLLECTING'] = 'Start Collecting';
+$lang['BUTTON_STOP_COLLECTING'] = 'Stop Collecting';
+$lang['BUTTON_ADD_COLUMN'] = 'Add Column';
+$lang['BUTTON_DELETE_COLUMN'] = 'Delete';
+$lang['BUTTON_IMPORT_DATA'] = 'Import';
+$lang['BUTTON_EXPORT_DATA'] = 'Export';
+$lang['BUTTON_COMPARE'] = 'Compare';
+
+
+//Graph Properties
+$lang['GRAPH_PROPERTIES'] = 'Graph Properties';
+$lang['GRAPH_PROPERTIES_FUNCTION'] = 'Func:';
+
+// Add Column popup
+$lang['ADD_COLUMN_NOTE'] = '
+      - dis(x): Derivative of x
+      - inf(x): Integral of x
+      - const(A): Constant name A, 
+      which can be changed while drawing
+      - operation: + (add), -(sub), *(mul), / (div)';
+
+?>
---
+++ WebSensor/languages/lang.vn.php
@@ -0,0 +1,66 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+$lang['PAGE_TITLE'] = 'ViLabs';
+
+//General:
+$lang['PROPERTIES_NAME'] = 'Tên:';
+$lang['PROPERTIES_DESCRIPTION'] = 'Miêu Tả:';
+$lang['PROPERTIES_NOTE'] = 'Chú Thích:';
+
+//About
+$lang['INFORMATION_ABOUT'] = 'Thông tin';
+$lang['INFORMATION_UPDATE'] = 'Cập Nhật';
+$lang['INFORMATION_CHOOSE_FILE'] = 'Choose file';
+
+//System information
+$lang['SYSTEM_INFO'] = 'System Infos';
+$lang['SYSTEM_INFO_TYPE'] = 'Loại:';
+
+//Live View Properties
+$lang['LIVE_VIEW_PROPERTIES'] = 'Live view properties';
+
+//Column Properties
+$lang['COLUMN_PROPERTIES'] = 'Column Properties';
+$lang['COLUMN_PROPERTIES_TYPE'] = 'Type:';
+$lang['COLUMN_PROPERTIES_AVG'] = 'Avg:';
+$lang['COLUMN_PROPERTIES_MAX'] = 'Max:';
+$lang['COLUMN_PROPERTIES_MIN'] = 'Min:';
+$lang['COLUMN_PROPERTIES_AB_ER'] = 'Ab Er:';
+$lang['COLUMN_PROPERTIES_RE_ER'] = 'Re Er:';
+$lang['COLUMN_PROPERTIES_EQUATION'] = 'Equation:';
+
+//Tabs
+$lang['TAB_LIVEVIEW'] = 'Trực Tuyến';
+$lang['TAB_DATA_TABLE'] = 'Dữ Liệu';
+
+//Button
+$lang['BUTTON_ADD_GRAPH'] = 'Add Graph';
+$lang['BUTTON_START_COLLECTING'] = 'Start Collecting';
+$lang['BUTTON_STOP_COLLECTING'] = 'Stop Collecting';
+$lang['BUTTON_ADD_COLUMN'] = 'Add Column';
+$lang['BUTTON_DELETE_COLUMN'] = 'Delete';
+$lang['BUTTON_IMPORT_DATA'] = 'Import';
+$lang['BUTTON_EXPORT_DATA'] = 'Export';
+$lang['BUTTON_COMPARE'] = 'Compare';
+
+
+//Graph Properties
+$lang['GRAPH_PROPERTIES'] = 'Graph Properties';
+$lang['GRAPH_PROPERTIES_FUNCTION'] = 'Func:';
+
+// Add Column popup
+$lang['ADD_COLUMN_NOTE'] = '
+      - dis(x): Derivative of x
+      - inf(x): Integral of x
+      - const(A): Constant name A, 
+      which can be changed while drawing
+      - operation: + (add), -(sub), *(mul), / (div)';
+
+?>
