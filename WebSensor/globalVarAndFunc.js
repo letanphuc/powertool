8e9bc1f7af05118c46cef3807850a18a0f672558
duydn1 <duy.dn89@gmail.com>
2016-10-20 09:05:25 +0700

    Fix lagging when switching tabs

--- WebSensor/globalVarAndFunc.js
+++ WebSensor/globalVarAndFunc.js
@@ -1,11 +1,13 @@
          // Global Variables
     var arrayColorForLiveView =["#ff0000", "#00ff00", "#0000ff", "#000000", "#ffff00", "#ff8c1a", "#660066"];
     var liveViewSelected =[];
+    var liveViewSelectedOld =[];
     var maginData = [0,0,0,0,0,0,0];
     var focusTab = 0;         //
     var dpsForLiveView = []; // dataPoints
     var matrix = [];     // content values of table
     var recordId = [];  // content record id
+    var totalSensorAvailable = 0; // total sensor plugin.
     var totalValueRow = 0;    // total rows of table (get from database)
     var tableLable =[];       // content lable of table include RECORDID
     var columnInfo =[];       // show information of column which is sellected
@@ -14,16 +16,18 @@
     var arrayGraphValue =[];  // to get value from add graph dialog to Graph Info
     var GraphInfo = [];       // show info of graph
     var tabIndex = 0;
-    var totalSensor = 0
+    var totalSensor = 0;
     var needToReloadTable = false;
     var MAX_AVAILABLE_SENSORS = 4
         //init value
         for (i = 0; i < MAX_AVAILABLE_SENSORS; i++) { 
           liveViewSelected[i] = false;
+		  liveViewSelectedOld[i] = false;
         }
         function updateLiveViewValues () {
-          for (i = 0; i < realtimeSensorInfo.length; i++) { 
-            liveViewSelected[realtimeSensorInfo[i].sensorID] = document.getElementById('liveViewCheckBox'+i).checked;
+          if(window.lastFocusStatus)
+          	for (i = 0; i < realtimeSensorInfo.length; i++) { 
+            	liveViewSelected[realtimeSensorInfo[i].sensorID] = document.getElementById('liveViewCheckBox'+i).checked;
           }
         }
     function resizeIframe(obj) {
@@ -96,62 +100,62 @@
 			cell.innerHTML = newArray[i - 1]
 		}
 	}
-        function sum(array)
-        {
-			var total = 0;
-			array.forEach( function (item, index){
-				 total+=item;
-				 });
-			return total;
-        }
-        function suggestSinExpression(arr){
-        	/** Asin(Bx + C) + D */ 
-        	var result = []
-        	var firsrZero = -1;
-        	var secondZero = -1;
-        	var A = 0;
-        	var B = 0;
-        	var C = 0;
-        	var D = 0;
+    function sum(array)
+    {
+		var total = 0;
+		array.forEach( function (item, index){
+			 total+=item;
+			 });
+		return total;
+    }
+    function suggestSinExpression(arr){
+    	/** Asin(Bx + C) + D */ 
+    	var result = []
+    	var firsrZero = -1;
+    	var secondZero = -1;
+    	var A = 0;
+    	var B = 0;
+    	var C = 0;
+    	var D = 0;
 
-        	var min = arr[0];
-        	var max = arr[0];
-        	var total = sum(arr)
-        	var average = total *1.0 / arr.length;
-        	console.log(average)
+    	var min = arr[0];
+    	var max = arr[0];
+    	var total = sum(arr)
+    	var average = total *1.0 / arr.length;
+    	console.log(average)
 
-        	for (var i = 0; i < arr.length; i ++){
-        		if (arr[i] > max)
-        			max = arr[i];
-        		if (arr[i] < min)
-        			min = arr[i];
+    	for (var i = 0; i < arr.length; i ++){
+    		if (arr[i] > max)
+    			max = arr[i];
+    		if (arr[i] < min)
+    			min = arr[i];
 
-        		if ((arr[i] - average)*(arr[i + 1]-average) < 0 && (arr[i] < average)){
-        			/* Found zero */
-        			if (firsrZero != -1){
-        				secondZero = i;
-        				B = 2.0* math.PI / (secondZero - firsrZero);
-        				A = (max - min) / 2.0
-        				D = (max + min) / 2.0;
-        				C = math.asin(0) - B*i;
-                        B = B*30;
+    		if ((arr[i] - average)*(arr[i + 1]-average) < 0 && (arr[i] < average)){
+    			/* Found zero */
+    			if (firsrZero != -1){
+    				secondZero = i;
+    				B = 2.0* math.PI / (secondZero - firsrZero);
+    				A = (max - min) / 2.0
+    				D = (max + min) / 2.0;
+    				C = math.asin(0) - B*i;
+                    B = B*30;
 
-        				break;
-        			}
-        			else{
-        				firsrZero = i;
-        			}
-        		}
-        	}
-        	result['expression'] = "const(A)*sin(const(B)*time + const(C)) + const(D)"
-            result['A'] = math.round(A,4);
-            result['B'] = math.round(B,4);
-            result['C'] = math.round(C,4);
-            result['D'] = math.round(D,4);
+    				break;
+    			}
+    			else{
+    				firsrZero = i;
+    			}
+    		}
+    	}
+    	result['expression'] = "const(A)*sin(const(B)*time + const(C)) + const(D)"
+        result['A'] = math.round(A,4);
+        result['B'] = math.round(B,4);
+        result['C'] = math.round(C,4);
+        result['D'] = math.round(D,4);
 
 
-        	return result;
-        }
+    	return result;
+    }
 
 	function updateInfoGraph()
 	{
@@ -160,3 +164,46 @@
   function reloadInfoColumn () {
     $( "#infoLiveView" ).load( "Table/infoColumn.html" );
   }
+  
+  var selectColumnIndex = -1;
+  var selectRowIndex = -1;
+  function setIndexAndHighlight (){
+    console.log("setIndexAndHighlight is called")
+    $('#dataTableID th').each(function(index) {
+      $(this).click(function() {
+        selectColumnIndex = index;
+        selectRowIndex = -1;
+        if(index > 0){
+          columnSelected = selectColumnIndex -1;
+          window.reloadInfoColumn();
+        }
+        console.log(index);
+        $('tr').each(function() {
+          $(this).removeClass('rowHighlight');  
+          if(index > 0){
+            $(this).find('td').not(':eq(index)').removeClass('highlight');
+            $(this).find('th').not(':eq(index)').removeClass('highlight');
+            $(this).find('td').eq(index).toggleClass('highlight');
+            $(this).find('th').eq(index).toggleClass('highlight');
+          }
+        });
+      });
+    });
+  }
+  function setIndexAndHighlightRow(){
+    console.log("setIndexAndHighlightRow is called")
+    $(".firstRow").click(function(){
+      selectColumnIndex = -1;
+      $('#dataTableID th').each(function(index) {
+        $('tr').each(function() {
+          if(index > 0){
+            $(this).find('td').not(':eq(index)').removeClass('highlight');
+            $(this).find('th').not(':eq(index)').removeClass('highlight');
+          }
+        });
+      });
+      selectRowIndex = $(this).index();
+      console.log(" select row clicked selectRowIndex = " + selectRowIndex )
+      $(this).addClass('rowHighlight').siblings().removeClass('rowHighlight');    
+    });
+  };
